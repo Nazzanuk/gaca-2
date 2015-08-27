5bf7acb3da1d4ed53fd7b1d67625baea9e7b93db
Nathan N <nazzanuk@gmail.com>
2015-08-27 08:19:19 +0100

    dynamic get in touch

--- src/components/home-boxes/home-boxes.js
+++ src/components/home-boxes/home-boxes.js
@@ -64,4 +64,35 @@
 
         init();
     }]);
+
+    app.controller('GetInTouchCtrl', ['PopupService', '$scope', '$element', '$timeout', function (PopupService, $scope, $element, $timeout) {
+
+        $scope.contactSector = '';
+        $scope.contactDepartment = '';
+
+        var sectors = {
+            'air-nav':{
+                'sector-1':"Sector 1",
+                'sector-2':"Sector 2",
+                'sector-3':"Sector 3",
+                'sector-4':"Sector 4"
+            },
+            'safety-org':{
+                'sector-5':"Sector 5",
+                'sector-6':"Sector 6",
+                'sector-7':"Sector 7"
+            }
+        };
+
+        var getDepartments = function () {
+            return sectors[$scope.contactSector];
+        };
+
+        var init = function () {
+        };
+
+        init();
+
+        $scope.getDepartments = getDepartments;
+    }]);
 }());
