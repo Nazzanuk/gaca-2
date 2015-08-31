48be96128e477e35bd2376a09f070d678d40759b
Nathan N <nazzanuk@gmail.com>
2015-08-31 11:35:44 +0100

    added search javascript and more arabic css fixes

--- release/public/app.js
+++ release/public/app.js
@@ -2,6 +2,51 @@ var app = angular.module('app', []);
 
 
 (function () {
+    app.controller('ECatalogueCtrl', ['$scope', '$element', '$interval', function ($scope, $element, $interval) {
+        var eServices = [];
+
+        var genEServices = function () {
+            for (var i = 1;i <=100; i++) {
+                eServices.push({
+                    id:  _.random(1000, 9999),
+                    title:_.sample(['Export of', 'Statement of', 'Regarding', 'Information Required']) + " of " + " "
+                        + _.sample([
+                            'Wheel Maintenance of TSR',
+                            'Inspection Program',
+                            'ELT Update',
+                            'Incident Help',
+                            'Charter Flights',
+                            'General Maintenance',
+                            'Scheduled Flights'
+                        ]),
+                    popularity: _.random(0, 10),
+                    date:_.random(2000, 2015) * 10000 + _.random(1, 1) * 100 +  _.random(1, 28),
+                    audience:_.sample(['Pilot', 'Airman', 'Aircraft', 'Airlines', 'Airports', 'Training Centres']),
+                    sector:_.sample(["Air Navigation Services", 'Information Technology', 'Finance & Admin', 'International Organisation'])
+                });
+            };
+
+        };
+
+        var getEServices = function (filter, sort) {
+            console.log(filter, sort);
+            var filteredList = _.where(eServices, filter, sort);
+            var sortedList = _.sortBy(filteredList, sort);
+            return _.first(sortedList, 5);
+        };
+
+        var init = function () {
+            genEServices();
+        };
+
+        init();
+
+        $scope.getEServices = getEServices;
+
+    }]);
+}());
+
+(function () {
     app.controller('ESearchCtrl', ['$scope', function ($scope) {
 
         $scope.types = ["E-Services"];
@@ -160,51 +205,6 @@ var app = angular.module('app', []);
                 $('.result').velocity('stop').velocity('transition.flipYIn', {stagger: 50});
             }, 50)
         }
-
-    }]);
-}());
-
-(function () {
-    app.controller('ECatalogueCtrl', ['$scope', '$element', '$interval', function ($scope, $element, $interval) {
-        var eServices = [];
-
-        var genEServices = function () {
-            for (var i = 1;i <=100; i++) {
-                eServices.push({
-                    id:  _.random(1000, 9999),
-                    title:_.sample(['Export of', 'Statement of', 'Regarding', 'Information Required']) + " of " + " "
-                        + _.sample([
-                            'Wheel Maintenance of TSR',
-                            'Inspection Program',
-                            'ELT Update',
-                            'Incident Help',
-                            'Charter Flights',
-                            'General Maintenance',
-                            'Scheduled Flights'
-                        ]),
-                    popularity: _.random(0, 10),
-                    date:_.random(2000, 2015) * 10000 + _.random(1, 1) * 100 +  _.random(1, 28),
-                    audience:_.sample(['Pilot', 'Airman', 'Aircraft', 'Airlines', 'Airports', 'Training Centres']),
-                    sector:_.sample(["Air Navigation Services", 'Information Technology', 'Finance & Admin', 'International Organisation'])
-                });
-            };
-
-        };
-
-        var getEServices = function (filter, sort) {
-            console.log(filter, sort);
-            var filteredList = _.where(eServices, filter, sort);
-            var sortedList = _.sortBy(filteredList, sort);
-            return _.first(sortedList, 5);
-        };
-
-        var init = function () {
-            genEServices();
-        };
-
-        init();
-
-        $scope.getEServices = getEServices;
 
     }]);
 }());
