8a5c4cbc0285eef6c4bafcdaff89b6f3bc97b22c
Nathan <nazzanuk@gmail.com>
2015-12-29 17:10:27 +0000

    gaca design updates

--- release/public/app.js
+++ release/public/app.js
@@ -547,6 +547,50 @@ app.controller('ContactCtrl', function ($scope, $timeout, $http) {
     }]);
 }());
 
+app.controller('MenuCtrl', ['$scope', function ($scope) {
+
+    var showing = false;
+
+    var toggleSidebar = function () {
+        if (showing) hideSidebar();
+        else showSidebar();
+    };
+
+    var showSidebar = function () {
+        $('.content-area, .menu').addClass('active');
+        $('.menu-overlay').addClass('active');
+        showing = true;
+    };
+
+    var hideSidebar = function () {
+        $('.content-area, .menu').removeClass('active');
+        $('.menu-overlay').removeClass('active');
+        showing = false;
+    };
+
+    var events = function () {
+        $(document).on('click', '.header-menu', toggleSidebar);
+        $(document).on('click', '.menu-overlay, .header-close', hideSidebar);
+    };
+
+    var init = function () {
+        events();
+    };
+
+    init();
+
+}]);
+
+app.controller('MenuItemCtrl', ['$scope', function ($scope) {
+
+    $scope.active;
+
+    $scope.toggleActive = function () {
+        $scope.active = !$scope.active;
+    };
+
+}]);
+
 (function () {
     app.controller('PopupCtrl', ['$scope', 'PopupService', '$sce', function ($scope, PopupService, $sce) {
 
@@ -622,50 +666,6 @@ app.controller('ContactCtrl', function ($scope, $timeout, $http) {
         that.getPopupHeader = getPopupHeader;
     }]);
 }());
-
-app.controller('MenuCtrl', ['$scope', function ($scope) {
-
-    var showing = false;
-
-    var toggleSidebar = function () {
-        if (showing) hideSidebar();
-        else showSidebar();
-    };
-
-    var showSidebar = function () {
-        $('.content-area, .menu').addClass('active');
-        $('.menu-overlay').addClass('active');
-        showing = true;
-    };
-
-    var hideSidebar = function () {
-        $('.content-area, .menu').removeClass('active');
-        $('.menu-overlay').removeClass('active');
-        showing = false;
-    };
-
-    var events = function () {
-        $(document).on('click', '.header-menu', toggleSidebar);
-        $(document).on('click', '.menu-overlay, .header-close', hideSidebar);
-    };
-
-    var init = function () {
-        events();
-    };
-
-    init();
-
-}]);
-
-app.controller('MenuItemCtrl', ['$scope', function ($scope) {
-
-    $scope.active;
-
-    $scope.toggleActive = function () {
-        $scope.active = !$scope.active;
-    };
-
-}]);
 
 
 (function () {
